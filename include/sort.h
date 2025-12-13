3d0a4eae260734f61c05d0fb9ec7cf0b59333b6e
Irfan Nazir <179750083+irrfannazir@users.noreply.github.com>
2025-12-13 19:50:21 +0530

    sort.h

---
+++ include/sort.h
@@ -0,0 +1,38 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdlib.h>
+
+
+// Radix Sort
+static inline int __getMax__(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+static inline void countSort(int arr[], int n, int exp) {
+    int count[10] = {0};
+    int *output = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+    free(output);
+}
+
+static inline void __radix__(int arr[], int n) {
+    int max = __getMax__(arr, n);
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
+#endif
